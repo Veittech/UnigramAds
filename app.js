7440a6ef67abafe762b4bd356b1d6b0cf4805249
Mr_Veit <Misster.veit@gmail.com>
2025-04-24 23:17:04 +1200

    Minor update

--- app.js
+++ app.js
@@ -47,7 +47,7 @@
     streamingAssetsUrl: "StreamingAssets",
     companyName: "Veittech",
     productName: "Unigram Ads",
-    productVersion: "1.0",
+    productVersion: "1.0.3",
     showBanner: unityShowBanner,
   };
 
